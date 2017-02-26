20631f51115ac4f255c99972bfe35a3e3eae5e27
Sarthak-04 <4sarthakagrawal@gmail.com>
2017-02-26 14:03:24 +0530

    Adding pages

---
+++ js/script-main.js
@@ -0,0 +1,32 @@
+// Skills progressbar animation
+
+$(function() {
+  	function loadProgressBars() {
+        $('.progress-bar').each(function() {
+            var bar_value = $(this).attr('aria-valuenow') + '%';                
+    		$(this).animate({ width: bar_value }, { duration: 1000, easing: 'easeOutCirc' });
+        });
+    }
+
+	$(document).bind('scroll', function(ev) {
+	    var scrollOffset = $(document).scrollTop();
+	    var containerOffset = $('#skills').offset().top - window.innerHeight;
+	    if (scrollOffset > containerOffset) {
+        	loadProgressBars();
+        	// unbind event not to load scrolsl again
+        	$(document).unbind('scroll');
+    	}
+    });
+});
+
+function hoverAbtImage(element) {
+	element.setAttribute('src', 'static/card_cart.jpg')
+}
+
+function unhoverAbtImage(element) {
+	element.setAttribute('src', 'static/card_img1.jpg')
+}
+
+ $('.flip').click(function(){
+        $(this).find('.card').toggleClass('flipped');
+ });
